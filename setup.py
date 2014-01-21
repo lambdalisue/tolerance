e8d1c0d0d8bcfd7d9138e70ba1b442ec6c7a5a33
Λlisue <lambdalisue@hashnote.net>
2014-01-21 14:33:46 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 NAME = 'tolerance'
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 def read(filename):
     import os
