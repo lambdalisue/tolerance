1ea675f62d1a26f85f6386081532b2cd8d06c24f
lambdalisue <lambdalisue@hashnote.net>
2017-07-14 19:35:21 +0900

    Explicitly specify encoding

--- setup.py
+++ setup.py
@@ -8,16 +8,18 @@ VERSION = '0.1.2'
 
 def read(filename):
     import os
+    from io import open
     BASE_DIR = os.path.dirname(__file__)
     filename = os.path.join(BASE_DIR, filename)
-    fi = open(filename, 'r')
-    return fi.read()
+    with open(filename, 'r', encoding='utf-8') as fi:
+        return fi.read()
 
 
 def readlist(filename):
     rows = read(filename).split("\n")
     rows = [x.strip() for x in rows if x.strip()]
     return list(rows)
+
 
 setup_extras = {}
 if sys.version_info > (3,):
@@ -28,8 +30,8 @@ setup(
     version=VERSION,
     description=('A function decorator which makes a function tolerant ('
                  'the function fail silently).'),
-    long_description = read('README.rst'),
-    classifiers = (
+    long_description=read('README.rst'),
+    classifiers=(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
@@ -42,16 +44,16 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ),
-    keywords = 'function decorator, decorator, fail silently',
-    author = 'Alisue',
-    author_email = 'lambdalisue@hashnote.net',
-    url = 'https://github.com/lambdalisue/%s' % NAME,
-    download_url = 'https://github.com/lambdalisue/%s/tarball/master' % NAME,
-    license = 'MIT',
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    include_package_data = True,
-    package_data = {
+    keywords='function decorator, decorator, fail silently',
+    author='Alisue',
+    author_email='lambdalisue@hashnote.net',
+    url='https://github.com/lambdalisue/%s' % NAME,
+    download_url='https://github.com/lambdalisue/%s/tarball/master' % NAME,
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    package_data={
         '': ['LICENSE',
              'README.rst',
              'TEST.rst',
